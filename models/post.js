39a210c40b135707d6ae12002c0571ecad6ee730
Jongheon Kim <sapzape@gmail.com>
2019-08-08 00:43:13 +0900

    Add post write / search function

--- models/post.js
+++ models/post.js
@@ -4,35 +4,41 @@ const postSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
   contents: { type: String, required: true },
   createTime: { type: Date, default: Date.now },
-  creator: [{
+  creator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
-  }],
-  from: [{
+  },
+  from: {
      type: mongoose.Schema.Types.ObjectId,
      ref: "School"
-  }]
+  }
 });
 
+// Create new user document
 postSchema.statics.create = function (payload) {
   const user = new this(payload);
   return user.save();
 };
 
+// Find all
 postSchema.statics.findAll = function () {
   return this.find({});
 };
 
+// Find one by UserId
 postSchema.statics.findOneByUserId = function(userId) {
   return this.findOne({ userId });
 };
 
+// Update by UserId
 postSchema.statics.updateByUserId = function (userId, payload) {
   return this.findOneAndUpdate({ userId }, payload, { new: true });
 };
 
+// Delete by UserId
 postSchema.statics.deleteByUserId = function (userId) {
   return this.remove({ userId });
 };
 
+// Create model & export
 module.exports = mongoose.model('Post', postSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,36 @@
 const router = require('express').Router();
+const Post = require('../models/post');
+const User = require('../models/user');
+const School = require('../models/school');
+const paramHandler = require('../helpers/paramHandler');
+const whiteList = ['userId', 'schoolName', 'contents'];
 
-router.get('/', (req, res) => {
-  res.send(true);
+// 글 조회
+router.get('/', async (req, res) => {
+  try {
+    const post = await Post.findAll()
+    if (!post) return res.status(404).send({ err: 'Post not found' });
+    res.status(200).send({ success: true, message: '', data: post });
+  } catch(err) {
+    res.status(500).send({ success: false, message: err.toString() });
+  }
+});
+
+// 글쓰기
+router.post('/', async (req, res) => {
+  try {
+    const params = paramHandler.filterParams(req.body, whiteList);
+    const user = await User.findOneByUserId(params.userId)
+    if (!user) res.status(404).send({ success: false, message: "User not found", data: null });
+
+    const school = await School.findOneBySchoolName(params.schoolName);
+    if (!school) res.status(404).send({ success: false, message: "School not found", data: null });
+
+    const newPost = await Post.create(params);
+    res.status(201).send({ success: true, message: 'Post Successfully created', data: newPost });
+  } catch (err) {
+    res.status(500).send({ success: false, message: err.toString() });
+  }
 });
 
 module.exports = router;
