65a1eb3f3e6abc323f9629c67e8c40b6ec172560
Jongheon Kim <sapzape@gmail.com>
2019-08-08 00:40:28 +0900

    Added user inquiry / creation function

--- models/user.js
+++ models/user.js
@@ -3,40 +3,38 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
   userId: { type: String, required: true, unique: true },
-  createTime: { type: Date },
+  createTime: { type: Date, default: Date.now },
   posts: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post"
   }],
   likeSchool: [{
-    id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "School"
-  },
-  createTime: { type: Date, default: Date.now },
-  deleteTime: { type: Date, default: null },
+    school : {type: mongoose.Schema.Types.ObjectId, ref: "School"},
+    createTime: { type: Date },
+    endTime: { type: Date }
   }]
 });
 
+// Create new user document
 userSchema.statics.create = function (payload) {
   const user = new this(payload);
   return user.save();
 };
 
-userSchema.statics.findAll = function () {
-  return this.find({});
-};
-
+// Find one by UserId
 userSchema.statics.findOneByUserId = function(userId) {
   return this.findOne({ userId });
 };
 
-userSchema.statics.updateByUserId = function (userId, payload) {
-  return this.findOneAndUpdate({ userId }, payload, { new: true });
+// Update by UserId
+userSchema.statics.updateByUserId = function (userId, school) {
+  return this.findOneAndUpdate({ userId }, {$addToSet: school}, { new: true });
 };
 
-userSchema.statics.deleteByUserId = function (userId) {
-  return this.remove({ userId });
+// Update by UserId
+userSchema.statics.unlikeByUserId = function (userId, school) {
+  return this.findOneAndUpdate({ userId }, {$pull: school}, { new: true });
 };
 
+// Create model & export
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const User = require('../models/user');
+const School = require('../models/school');
+const paramHandler = require('../helpers/paramHandler');
+const whiteList = ['userId', 'post', 'likeSchool', 'schoolName'];
 
-router.get('/', (req, res) => {
-  res.send(true)
+// 유저 조회
+router.get('/:userId', async (req, res) => {
+  try {
+    const params = paramHandler.filterParams(req.params, whiteList);
+    const user = await User.findOneByUserId(params.userId);
+    if (!user) return res.status(404).send({ err: 'User not found' });
+    res.status(200).send({ success: true, message: '', data: user });
+  } catch(err) {
+    res.status(500).send({ success: false, message: err.toString() });
+  }
+});
+
+// 유저 생성
+router.post('/', async (req, res) => {
+  try {
+    const params = paramHandler.filterParams(req.body, whiteList);
+    const newUser = await User.create(params);
+    res.status(201).send({ success: true, message: 'User Successfully created', data: newUser });
+  } catch (err) {
+    res.status(500).send({ success: false, message: err.toString() });
+  }
 });
 
 module.exports = router;
