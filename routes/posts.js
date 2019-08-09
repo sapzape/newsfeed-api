c16bfacb8ed602608f731953a446354ee0ea5656
Jongheon Kim <sapzape@gmail.com>
2019-08-10 02:55:34 +0900

    Edit to receive feeds for subscription start / end periods

--- routes/posts.js
+++ routes/posts.js
@@ -4,15 +4,16 @@ const User = require('../models/user');
 const School = require('../models/school');
 const Follow = require('../models/follow');
 const paramHandler = require('../helpers/paramHandler');
-const whiteList = ['userId', 'schoolName', 'contents'];
+const whiteList = ['userId', 'contents', 'schoolName', 'region'];
 
 router.get('/', async (req, res) => {
   try {
     const post = await Post.findAll()
     if (!post) return res.status(404).send({ err: 'Post not found' });
-    res.status(200).send({ success: true, message: '', data: post });
+    
+    return res.status(200).send({ success: true, message: '', data: post });
   } catch(err) {
-    res.status(500).send({ success: false, message: err.toString() });
+    return res.status(500).send({ success: false, message: err.toString() });
   }
 });
 
@@ -20,20 +21,19 @@ router.post('/', async (req, res) => {
   try {
     const params = paramHandler.filterParams(req.body, whiteList);
     const user = await User.findOneByUserId(params.userId)
-    if (!user) res.status(404).send({ success: false, message: "User not found", data: null });
+    if (!user) return res.status(404).send({ success: false, message: "User not found", data: null });
 
-    const school = await School.findOneBySchoolInfo({schoolName: params.schoolName});
-    if (!school) res.status(404).send({ success: false, message: "School not found", data: null });
+    const school = await School.findOneBySchoolInfo({schoolName: params.schoolName, region: params.region});
+    if (!school) return res.status(404).send({ success: false, message: "School not found", data: null });
 
     const newPost = await Post.create({contents: params.contents, creator: user._id, from: school._id});
-    res.status(201).send({ success: true, message: 'Post Successfully created', data: newPost });
+    return res.status(201).send({ success: true, message: 'Post Successfully created', data: newPost });
   } catch (err) {
-    res.status(500).send({ success: false, message: err.toString() });
+    return res.status(500).send({ success: false, message: err.toString() });
   }
 });
 
-//todo(jhkim) 구독 중인 학교 별로 조회시, 구독 시작/ 종료 시점 조건 추가 필요
-//todo(jhkim) offset / limit 설정 필요
+//todo(jhkim) offset / limit 도 전달 받을 수있도록 수정 필요
 router.get('/:userId', async (req, res) => {
  try {
    const params = paramHandler.filterParams(req.params, whiteList);
@@ -41,16 +41,17 @@ router.get('/:userId', async (req, res) => {
    if (!user) return res.status(404).send({ err: 'User not found' });
 
    const follows = await Follow.findByUserId({userId: user._id}).populate('userId').populate('subscribeTo');
-   if (!follows) res.status(404).send({ err: '구독 중인 학교 없음' });
+   if (!follows) return res.status(404).send({ err: '구독 중인 학교 없음' });
 
    let postList = [];
    for (let follow of follows) {
-    let posts = await Post.findPosts({from: follow.subscribeTo._id}).populate('creator').populate('from');
+    let endTime = (follow.endFollow === null) ? Date.now() : follow.endFollow;
+    let posts = await Post.findPosts({from: follow.subscribeTo._id, createTime: {"$gte": follow.startFollow, "$lt": endTime}}).populate('creator').populate('from');
      if (posts) postList.push(...posts);
    }
-   res.status(201).send({ success: true, message: 'Post 조회', data: postList });
+   return res.status(201).send({ success: true, message: 'Post 조회', data: postList });
  } catch (err) {
-   res.status(500).send({ success: false, message: err.toString() });
+   return res.status(500).send({ success: false, message: err.toString() });
  }
 });
 
